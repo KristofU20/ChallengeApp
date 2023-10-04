41a6b936f45ff4f460207d732dadfda40a1fddef
USER <USER@DESKTOP-93BRQDO>
2023-10-04 21:30:22 +0200

    Zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Zadanie domowe z dnia 4
+
+var name = "Ewa";
+var sex = "Kobieta";
+var age = 33;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex != "Kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else if (sex != "Kobieta" && age > 30)
+{
+    Console.WriteLine("Mężczyzna po 30");
+}
+
+
