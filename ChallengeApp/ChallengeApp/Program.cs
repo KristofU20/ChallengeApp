5ae3744211d665c1b19aba9ff22167efde930661
USER <USER@DESKTOP-93BRQDO>
2023-10-31 19:45:54 +0100

    czyszczeni po zad 10

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -9,26 +9,9 @@ employee.AddGrade(6);
 
 
 var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
 
-Console.WriteLine("Dla while");
-Console.WriteLine($"Average: {statistics4.Average:N2}");
-Console.WriteLine($"Min: {statistics4.Min}");
-Console.WriteLine($"Max: {statistics4.Max}");
 
-Console.WriteLine("Dla do while");
-Console.WriteLine($"Average: {statistics3.Average:N2}");
-Console.WriteLine($"Min: {statistics3.Min}");
-Console.WriteLine($"Max: {statistics3.Max}");
-
-Console.WriteLine("Dla do for");
-Console.WriteLine($"Average: {statistics2.Average:N2}");
-Console.WriteLine($"Min: {statistics2.Min}");
-Console.WriteLine($"Max: {statistics2.Max}");
-
-Console.WriteLine("Dla do foeach");
+Console.WriteLine("Dla do foreach");
 Console.WriteLine($"Average: {statistics1.Average:N2}");
 Console.WriteLine($"Min: {statistics1.Min}");
 Console.WriteLine($"Max: {statistics1.Max}");
