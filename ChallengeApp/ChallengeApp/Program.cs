ea47e9360ebd5a22917ea29a3b52901095d676e6
USER <USER@DESKTOP-93BRQDO>
2023-11-12 13:38:13 +0100

    Zadanie domowe z dnia 16

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -11,31 +11,36 @@ employee.AddGrade(56);
 employee.AddGrade(35);
 employee.AddGrade(42);
 
-
-while (true)
-{
-    Console.WriteLine("Podaj kolejną ocenę pracownika: ");
-    var input = Console.ReadLine();
-    if(input == "q")
-    {
-        break;
-    }
-
-    try
-    {
-        employee.AddGrade(input);
-    }
-    catch(Exception e)
-    {
-        Console.WriteLine($"Exception catched: {e.Message}");
-    }
-}
-
 var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: { statistics.Average}");
 Console.WriteLine($"Min:  { statistics.Min}");
 Console.WriteLine($"Max:  { statistics.Max}");
 Console.WriteLine($"AvarageLetter: {statistics.AverageLetter}");
+
+//while (true)
+//{
+//    Console.WriteLine("Podaj kolejną ocenę pracownika: ");
+//    var input = Console.ReadLine();
+//    if(input == "q")
+//    {
+//        break;
+//    }
+
+//    try
+//    {
+//        employee.AddGrade(input);
+//    }
+//    catch(Exception e)
+//    {
+//        Console.WriteLine($"Exception catched: {e.Message}");
+//    }
+//}
+
+//var statistics = employee.GetStatistics();
+//Console.WriteLine($"Average: { statistics.Average}");
+//Console.WriteLine($"Min:  { statistics.Min}");
+//Console.WriteLine($"Max:  { statistics.Max}");
+//Console.WriteLine($"AvarageLetter: {statistics.AverageLetter}");
 
 
 
